cd9106b8d3bb48c2e9d0aa7eeb0b9fe939eb3181
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-15 03:57:09 +0600

    project setup ready and added firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhinSdqjhZOr_e9sKqmm6hAcDc6cthfEw",
+    authDomain: "react-firebase-authentic-eb0a8.firebaseapp.com",
+    projectId: "react-firebase-authentic-eb0a8",
+    storageBucket: "react-firebase-authentic-eb0a8.appspot.com",
+    messagingSenderId: "984767494462",
+    appId: "1:984767494462:web:fc64f7384fe7e8e1dc946f"
+};
+
+export default firebaseConfig;
