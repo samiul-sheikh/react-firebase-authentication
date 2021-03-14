601e570355d9212c7b16d3f2e09e8ff277c98e0c
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-15 04:37:11 +0600

    Install firebase, initialize firebase app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-    return (
-        <div>
 
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    return (
+        <div className="App">
+            <button>Sign In</button>
         </div>
     );
 }
